c5ae9353542259a40b34af4807d388acd681fa28
Pang Yan Han <pangyanhan@gmail.com>
2013-01-16 11:19:18 +0800

    TestModifies::test_modifies_single - add comments to summarize SIMPLE program
    The summary is in terms of the individual lines of the SIMPLE program,
    such as the type of statement they are and what variables are being
    modified by individual statements.
    
    Signed-off-by: Pang Yan Han <pangyanhan@gmail.com>

--- IntegrationTesting/TestModifies.cpp
+++ IntegrationTesting/TestModifies.cpp
@@ -105,7 +105,67 @@ void TestModifies::test_modifies_single()
         procedure cleanUp { \
           im = done; \
         }";
+
+    // procOne
+    // x, a, b, y, kerb, big, g2, xcz, well, vv, noway, hi, thank
+    // xyz, aa, yes, im, dont, xc,
+
+    // ascP
+    // xcz, a, well, x, vv, noway, hi, thank
+
+    // doSmth
+    // yes, im, dont
+
+    // cleanUp
+    // im
+
+    // procedure procOne
+    // stmt 1 [assign] - x
+    // stmt 2 [assign] - a
+    // stmt 3 [assign] - b
+    // stmt 4 [while] - y, kerb, big, a, g2, xcz, well, x, vv, noway, hi, thank,
+    //                  xyz, aa
+    // stmt 5 [assign] - y
+    // stmt 6 [if] - kerb, big, a
+    // stmt 7 [assign] - kerb
+    // stmt 8 [assign] - big
+    // stmt 9 [assign] - a
+    // stmt 10 [assign] - g2
+    // stmt 11 [if] - xcz, a, well, x, vv, noway, hi, thank, xyz, aa
+    // stmt 12 [call ascP] - xcz, a, well, x, vv, noway, hi, thank
+    // stmt 13 [assign] - xyz
+    // stmt 14 [assign] - aa
+    // stmt 15 [call doSmth] - yes, im, dont
+    // stmt 16 [assign] - well
+    // stmt 17 [if] - a
+    // stmt 18 [assign] - aa
+    // stmt 19 [assign] - aa
+    // stmt 20 [assign] - xc
+
+    // procedure ascP
+    // stmt 21 [if] - xcz, a, well, x, vv, noway
+    // stmt 22 [assign] - xcz
+    // stmt 23 [while] - a, well, x
+    // stmt 24 [while] - a, well
+    // stmt 25 [assign] - a
+    // stmt 26 [assign] - well
+    // stmt 27 [assign] - x
+    // stmt 28 [assign] - vv
+    // stmt 29 [assign] - noway
+    // stmt 30 [assign] - x
+    // stmt 31 [assign] - hi
+    // stmt 32 [assign] - thank
+
+    // procedure doSmth
+    // stmt 33 [assign] - yes
+    // stmt 34 [call cleanUp] - im
+    // stmt 35 [assign] - dont
+
+    // procedure cleanUp
+    // stmt 36 [assign] - im
+
     evaluator.parseSimple(simpleProg);
+    // Modifies(assign,var)
     queryStr = "assign a; variable v; Select v such that Modifies(a,v)";
     evaluator.evaluate(queryStr, resultList);
     stringSet = set<string>(resultList.begin(), resultList.end());
