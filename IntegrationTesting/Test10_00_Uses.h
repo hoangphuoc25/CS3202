9ababd16ebc7163fbb0257737515dc2e6bafdc8b
Pang Yan Han <pangyanhan@gmail.com>
2013-02-24 22:03:01 +0800

    Test10_00_Uses.h - add annotated program
    Signed-off-by: Pang Yan Han <pangyanhan@gmail.com>

--- IntegrationTesting/Test10_00_Uses.h
+++ IntegrationTesting/Test10_00_Uses.h
@@ -30,3 +30,96 @@ private:
 };
 
 #endif
+
+/*
+     procedure Abcd { \
+01     one = two + three; \
+02     cee = 26 + 61; \
+03     f = 123 - two + 7; \
+04     while whileVar { \
+05       eps = delta; \
+06       if ifVar then { \
+07         destruct = 71; \
+08         head = quarters; \
+09         if aaa then { \
+10           big = small + 515; \
+11           x = y; \
+           } else { \
+12           shoulders = up; \
+13           call recProc; \
+           } \
+14         seven = up; \
+         } else { \
+15         nothing = much; \
+         } \
+       } \
+16     fol = eol; \
+17     if thisNot then { \
+18       hoho = haha; \
+       } else { \
+19       while youThere { \
+20         google = this; \
+21         red = sky; \
+         } \
+22       aaa = xe5; \
+       } \
+23     fire = wall; \
+24     call thirdProc; \
+      } \
+      procedure TwoProng { \
+25      this = 151 + 61 * 4156; \
+26      erm = yes; \
+      } \
+      procedure thirdProc { \
+27      bottles = up; \
+28      beat = this; \
+29      while wall { \
+30        building = destruct; \
+        } \
+31      call pForce; \
+      } \
+      procedure pForce { \
+32      force = power; \
+33      strength = will; \
+34      if there then { \
+35        while yes { \
+36          while nope { \
+37            what = is - this; \
+38            no = way; \
+39            ss = 77; \
+40            call recProc; \
+            } \
+41          while nope { \
+42            what = isThis; \
+            } \
+43          journey = long; \
+          } \
+44        far = away; \
+        } else { \
+45        die = extinct; \
+46        while TT { \
+47          dont = know; \
+48          if qed then { \
+49            heck = this; \
+            } else { \
+50            who = cares; \
+51            if pForce then { \
+52              you = are; \
+53              at = google; \
+              } else { \
+54              you = are; \
+55              at = some - other * place - 14; \
+56              call recProc; \
+              } \
+57            done = deal; \
+            } \
+58          clean = slate; \
+          } \
+        } \
+59      follow = star; \
+      }
+      procedure recProc { \
+60      min = max; \
+61      high = five; \
+      }";
+*/
