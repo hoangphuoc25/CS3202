cb93cc42dbe0871681e83b9f9c5b0d8f07b1413b
Pang Yan Han <pangyanhan@gmail.com>
2013-03-13 17:11:58 +0800

    Allow program to work with AutoTester
    Signed-off-by: Pang Yan Han <pangyanhan@gmail.com>

--- AutoTester/source/TestWrapper.cpp
+++ AutoTester/source/TestWrapper.cpp
@@ -1,4 +1,9 @@
+#include <list>
+#include <string>
 #include "TestWrapper.h"
+
+using std::list;
+using std::string;
 
 // implementation code of WrapperFactory - do NOT modify the next 5 lines
 AbstractWrapper* WrapperFactory::wrapper = 0;
@@ -10,22 +15,27 @@ AbstractWrapper* WrapperFactory::createWrapper() {
 volatile bool TestWrapper::GlobalStop = false;
 
 // a default constructor
-TestWrapper::TestWrapper() {
+TestWrapper::TestWrapper(): evaluator()
+{
   // create any objects here as instance variables of this class
   // as well as any initialization required for your spa program
 }
 
 // method for parsing the SIMPLE source
-void TestWrapper::parse(std::string filename) {
+void TestWrapper::parse(string filename)
+{
 	// call your parser to do the parsing
-  // ...rest of your code...
+    // ...rest of your code...
+    this->evaluator.parseSimple_from_file(filename);
 }
 
 // method to evaluating a query
-void TestWrapper::evaluate(std::string query, std::list<std::string>& results){
-// call your evaluator to evaluate the query here
-  // ...code to evaluate query...
+void TestWrapper::evaluate(string query, list<string>& results)
+{
+    // call your evaluator to evaluate the query here
+    // ...code to evaluate query...
 
-  // store the answers to the query in the results list (it is initially empty)
-  // each result must be a string.
+    // store the answers to the query in the results list (it is initially empty)
+    // each result must be a string.
+    this->evaluator.evaluate(query, results);
 }
--- AutoTester/source/TestWrapper.h
+++ AutoTester/source/TestWrapper.h
@@ -1,26 +1,34 @@
-#ifndef TESTWRAPPER_H
-#define TESTWRAPPER_H
+#ifndef T4_TESTWRAPPER_H
+#define T4_TESTWRAPPER_H
 
 #include <string>
 #include <iostream>
 #include <list>
 
-// include your other headers here
 #include "AbstractWrapper.h"
+#include "../../SPA/QueryEvaluator.h"
 
-class TestWrapper : public AbstractWrapper {
- public:
-  // default constructor
-  TestWrapper();
+class TestWrapper: public AbstractWrapper {
+public:
+    /// Default constructor
+    TestWrapper();
+
+    /// Destructor
+    ~TestWrapper();
+
+    /// Method for parsing the SIMPLE source
+    /// @filename name of the source file
+    virtual void parse(std::string filename);
   
-  // destructor
-  ~TestWrapper();
-  
-  // method for parsing the SIMPLE source
-  virtual void parse(std::string filename);
-  
-  // method for evaluating a query
-  virtual void evaluate(std::string query, std::list<std::string>& results);
+    /// method for evaluating a query
+    /// @param query the PQL query string
+    /// @param results a reference to a list of strings for storing
+    ///                the evaluation results
+    virtual void evaluate(std::string query,
+            std::list<std::string>& results);
+
+private:
+    QueryEvaluator evaluator;
 };
 
 #endif
